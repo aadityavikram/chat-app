84cd737996ea50fb21266819306952a35c4e18cc
aadityavikram <aadityavikram628@gmail.com>
2020-10-20 22:09:06 +0530

    Removed timestamps due to region issues on Heroku and centered bot prompts and gave bot a name

--- src/app.js
+++ src/app.js
@@ -15,22 +15,23 @@ const port = process.env.PORT || 3000
 
 io.on('connection', (socket) => {
     console.log('New Connection!')
+    // moment().format('h:mm a') --> add this to 3rd params of every message call to get timestamps
     socket.on('joined', (username, room) => {
         const currentUser = userJoin(socket.id, username, room)
         socket.join(currentUser.room)
-        socket.emit('message', '', 'Welcome!', moment().format('h:mm a'), undefined)
-        socket.broadcast.to(currentUser.room).emit('message', '', currentUser.username + ' has joined the chat!', moment().format('h:mm a'), undefined)
+        socket.emit('message', 'VikBot', 'Welcome!', '', 'center')
+        socket.broadcast.to(currentUser.room).emit('message', 'VikBot', currentUser.username + ' has joined the chat!', '', 'center')
         io.to(currentUser.room).emit('roomUsers', roomUsers(currentUser.room))
     })
     socket.on('chatMessage', (msg) => {
         const currentUser = getCurrentUser(socket.id)
-        socket.emit('message', currentUser.username, msg, moment().format('h:mm a'), 'right')
-        socket.broadcast.to(currentUser.room).emit('message', currentUser.username, msg, moment().format('h:mm a'), 'left')
+        socket.emit('message', currentUser.username, msg, '', 'right')
+        socket.broadcast.to(currentUser.room).emit('message', currentUser.username, msg, '', 'left')
     })
     socket.on('disconnect', () => {
         const currentUser = userLeave(socket.id)
         if (currentUser) {
-            io.to(currentUser.room).emit('message', '', currentUser.username + ' has left the chat!', moment().format('h:mm a'), undefined)
+            io.to(currentUser.room).emit('message', 'VikBot', currentUser.username + ' has left the chat!', '', 'center')
             io.to(currentUser.room).emit('roomUsers', roomUsers(currentUser.room))
         }
     })
